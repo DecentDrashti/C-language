75868dba612a65c8d9e08ef61bd03ac806ee22e6
DecentDrashti <drashtiruparelia.678@gmail.com>
2024-05-05 17:29:10 +0530

    basic info

---
+++ Boolean/b1.c
@@ -0,0 +1,11 @@
+//C has a bool data type, which is known as booleans.
+//you must import the following header file to use it:
+//*#include <stdbool.h>
+//comparing values and variable
+#include <stdio.h>
+
+int main() {
+  printf("%d", 10 > 9);  // Returns 1 (true) because 10 is greater than 9
+  
+  return 0;
+}
---
+++ Variable/v1.c
@@ -0,0 +1,7 @@
+//Variables are containers for storing data values, like numbers and characters.(int,float,char,etc)
+//this is not possible in c
+int myNum = 15;
+printf(myNum);  // Nothing happens
+//here we need to use %d(format specifier or a place holder )
+
+//*If you don't want others (or yourself) to change existing variable values, you can use the const keyword.
